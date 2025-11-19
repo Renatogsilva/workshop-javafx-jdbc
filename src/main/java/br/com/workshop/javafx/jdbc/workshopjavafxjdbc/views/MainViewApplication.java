4ce4a47524dd41168598c235cc4b98f8151742e7
Renato Gomes <silvarenato180@gmail.com>
2025-11-19 00:22:37 -0300

    Main view design

--- src/main/java/br/com/workshop/javafx/jdbc/workshopjavafxjdbc/views/MainViewApplication.java
+++ src/main/java/br/com/workshop/javafx/jdbc/workshopjavafxjdbc/views/MainViewApplication.java
@@ -3,6 +3,7 @@ package br.com.workshop.javafx.jdbc.workshopjavafxjdbc.views;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,12 @@ public class MainViewApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
